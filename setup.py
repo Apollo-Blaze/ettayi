1d2ae40415d3dc6f5f88a6ee9f5467edccadafd5
Apollo-Blaze <srichandsureshrocks@gmail.com>
2024-11-28 21:49:06 +0530

    Final touches

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="ettayi",
     version="1.0.0",
-    author="Your Name",
-    author_email="your_email@example.com",
-    description="Ettayi Language Interpreter",
+    author="Apollo-Blaze",
+    author_email="srichandsureshrocks@gmail.com",
+    description="Ettayi Lang",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourgithubusername/ettayi",  # Update with your GitHub repo
+    url="https://github.com/Apollo-Blaze/ettayi",  
     packages=find_packages(),
     install_requires=["lark-parser>=0.12.0"],  # Add other dependencies if needed
     entry_points={
         "console_scripts": [
-            "ettayi=ettayi.cli:main",  # Maps the `ettayi` command to `main` in cli.py
+            "ettayi=ettayi.cli:main",  
         ],
     },
     classifiers=[
